2ce647b2ce65146ff91f674cbbf28736715c8903
FarrukhbekCAU <xurrafboy@gmail.com>
2024-12-17 13:41:42 +0500

    initial commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+
+const Color tdRed = Color.fromARGB(255, 222, 89, 80);
+const Color tdBlue = Color.fromARGB(255, 12, 113, 195);
+
+const Color tdBlack = Color.fromARGB(255, 19, 18, 18);
+const Color tdGrey = Color.fromARGB(255, 130, 129, 129);
+
+const Color 
